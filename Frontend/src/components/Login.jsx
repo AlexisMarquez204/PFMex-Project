40c6529a825ed88fc2f929dd6ca8d047f2afcef0
isai <imianisai102@gmail.com>
2026-02-16 00:11:41 -0600

    finalizacion de login

---
+++ Frontend/src/components/Login.jsx
@@ -0,0 +1,40 @@
+import "./Login.css";
+import imagen from "../assets/usuario.png";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="login-wrapper">
+      
+      
+      <div className="login-left">
+        <img src="" alt="Logo" className="login-logo" />
+      </div>
+
+   
+      <div className="login-card">
+
+        <img src={imagen} alt="Imagen" className="login-top-image" />
+
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Contraseña" />
+
+        <button className="btn-access">
+          Acceso
+        </button>
+
+        <button 
+          className="btn-register"
+          onClick={() => navigate("/registro")}
+        >
+          ¿No tienes cuenta?
+        </button>
+
+      </div>
+    </div>
+  );
+}
+
+export default Login;
